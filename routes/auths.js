3917ceceeb6754335217093f906d920bdb3656c7
limbanga <95570833+limbanga@users.noreply.github.com>
2024-09-07 05:39:01 +0700

    add login form

---
+++ routes/auths.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/login', function(req, res, next) {
+  res.render('auths/login', { title: 'Express', users: [
+    { name: 'John', email: ''},
+    { name: 'Paul', email: ''},
+    { name: 'George', email: ''},
+    { name: 'Ringo', email: ''}
+    
+  ] });
+});
+
+module.exports = router;
