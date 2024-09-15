339c580c2ac662f9e7e9e267f855330d989bd2c4
limbanga <95570833+limbanga@users.noreply.github.com>
2024-09-16 00:05:14 +0700

    update note-card

---
+++ public/javascripts/popover.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+  const popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl);
+  });
+});
